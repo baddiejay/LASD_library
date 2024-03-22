d1e7a17511f73322ab14c83b430f7c2046cfaa59
baddiejay <chiaracoppola99@gmail.com>
2024-03-22 17:30:44 +0100

    Beginning of StackVec

--- Exercise1/stack/vec/stackvec.hpp
+++ Exercise1/stack/vec/stackvec.hpp
@@ -26,13 +26,14 @@ private:
 protected:
 
   using Vector<Data>::size;
-
-  // ...
+  //Points to the first available cell
+  unsigned long index = 0;
 
 public:
 
   // Default constructor
-  StackVec() = default;
+  //The stack is empty but the array has already one or more cells allocated in order to avoid to do an expansion on the first push
+  StackVec();
 
   /* ************************************************************************ */
 
@@ -71,33 +72,42 @@ public:
 
   // Specific member functions (inherited from Stack)
 
-  // type Top() specifiers; // Override Stack member (non-mutable version; must throw std::length_error when empty)
-  // type Top() specifiers; // Override Stack member (non-mutable version; must throw std::length_error when empty)
-  // type Pop() specifiers; // Override Stack member (must throw std::length_error when empty)
-  // type TopNPop() specifiers; // Override Stack member (must throw std::length_error when empty)
-  // type Push(argument) specifiers; // Override Stack member (copy of the value)
-  // type Push(argument) specifiers; // Override Stack member (move of the value)
+  //Accessing the cell with index-1 from the the pointer to the available cell
+  const Data& Top() const override; // Override Stack member (non-mutable version; must throw std::length_error when empty)
+  Data& Top() override; // Override Stack member (non-mutable version; must throw std::length_error when empty)
+  //If I'm using only a small fraction of my space then I have to shrink my vector
+  void Pop() override; // Override Stack member (must throw std::length_error when empty)
+  Data TopNPop() override; // Override Stack member (must throw std::length_error when empty)
+  //Insert in the place con the index and then incrementing the index 
+  //If the available space is not enough we have to extend the vector before inserting
+
+  void Push(const Data&) override; // Override Stack member (copy of the value)
+  void Push(Data&&) override; // Override Stack member (move of the value)
 
   /* ************************************************************************ */
 
   // Specific member functions (inherited from Container)
 
-  // type Empty() specifiers; // Override Container member
+  //The meaning is that no data is in the stack, not that the array has dimension 0
+  inline bool Empty() const noexcept override; // Override Container member
 
-  // type Size() specifiers; // Override Container member
+  //Size doesn't correspond to the allocated memory cells but to how many elements are in the stack
+  inline unsigned long Size() const noexcept override; // Override Container member
 
   /* ************************************************************************ */
 
   // Specific member function (inherited from ClearableContainer)
 
-  // type Clear() specifiers; // Override ClearableContainer member
+  //In this case I don't want to empty all the array making it of dimension 0, INSTEAD
+  //I want to leave some space in order to be ablre to do some pushes without resizing the vector
+  vodi Clear() override; // Override ClearableContainer member
 
 protected:
 
   // Auxiliary member functions
 
-  // type Expand() specifiers;
-  // type Reduce() specifiers;
+  void Expand(unsigned long int) noexcept;
+  void Reduce(unsigned long int) noexcept;
 
 };
 

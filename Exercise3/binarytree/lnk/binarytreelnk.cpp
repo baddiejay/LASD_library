eeaf3884f92a186a53d20493cb9b2936a25fdb4d
Chiara Coppola <80648900+baddiejay@users.noreply.github.com>
2024-06-03 16:25:41 +0200

    Update binarytreelnk.cpp

--- Exercise3/binarytree/lnk/binarytreelnk.cpp
+++ Exercise3/binarytree/lnk/binarytreelnk.cpp
@@ -52,23 +52,19 @@ bool BinaryTreeLnk<Data>::NodeLnk::HasRightChild() const noexcept {
     return (rightChild!=nullptr);
 }
 
-
 template <typename Data>
 const BinaryTreeLnk<Data>::NodeLnk& BinaryTreeLnk<Data>::NodeLnk::LeftChild() const {
     if(!HasLeftChild()) {
         throw std::out_of_range("No left child");
     }
-
     return *leftChild;
 }
-
 
 template <typename Data>
 const BinaryTreeLnk<Data>::NodeLnk& BinaryTreeLnk<Data>::NodeLnk::RightChild() const { 
     if(!HasRightChild()) {
         throw std::out_of_range("No right child");
     }
-
     return *rightChild;
 }
 
@@ -77,7 +73,6 @@ BinaryTreeLnk<Data>::NodeLnk& BinaryTreeLnk<Data>::NodeLnk::LeftChild() {
     if(!HasLeftChild()) {
         throw std::out_of_range("No left child");
     }
-
     return *leftChild;
 }
 
@@ -90,32 +85,24 @@ BinaryTreeLnk<Data>::NodeLnk& BinaryTreeLnk<Data>::NodeLnk::RightChild() {
 }
 
 //*********** BINARY TREE **************
+
 template <typename Data>
 BinaryTreeLnk<Data>::BinaryTreeLnk(const TraversableContainer<Data> & container) {
-
     size = container.Size();
-
-    QueueLst<NodeLnk*> queue;
+    QueueLst<NodeLnk*> queue;	//Use a queue to keep track of nodes that have yet to receive children
 
     container.Traverse([this, &queue](const Data &newData) {
-
-        
-        if (root==nullptr) {
+        if (root==nullptr) {	//If the tree is empty, this is the root
             root = new NodeLnk(newData);
             queue.Enqueue(root);
-
         } else if (!queue.Empty()){
-
             if (queue.Head()->leftChild == nullptr) {
-
                 queue.Head()->leftChild = new NodeLnk(newData);
                 queue.Enqueue(queue.Head()->leftChild);
-
             } else {
-
                 queue.Head()->rightChild = new NodeLnk(newData);
                 queue.Enqueue(queue.Head()->rightChild);
-                queue.Dequeue();
+                queue.Dequeue();  //The head node is removed from the tail since both its children are now assigned.
             }
         }
 
@@ -124,27 +111,18 @@ BinaryTreeLnk<Data>::BinaryTreeLnk(const TraversableContainer<Data> & container)
 
 template <typename Data>
 BinaryTreeLnk<Data>::BinaryTreeLnk(MappableContainer<Data> && container) noexcept{
-
     std::swap(size, container.Size());
-
     QueueLst<NodeLnk*> queue;
 
     container.Map([this, &queue](Data &newData) {
-
-        
         if (root==nullptr) {
             root = new NodeLnk(std::move(newData));
             queue.Enqueue(root);
-
         } else if (!queue.Empty()){
-
             if (queue.Head()->leftChild == nullptr) {
-
                 queue.Head()->leftChild = new NodeLnk(std::move(newData));
                 queue.Enqueue(queue.Head()->leftChild);
-
             } else {
-
                 queue.Head()->rightChild = new NodeLnk(std::move(newData));
                 queue.Enqueue(queue.Head()->rightChild);
                 queue.Dequeue();
@@ -158,71 +136,52 @@ BinaryTreeLnk<Data>::~BinaryTreeLnk() {
     Clear();
 }
 
-
 template <typename Data>
 BinaryTreeLnk<Data>::NodeLnk* BinaryTreeLnk<Data>::recursiveCopyTree(NodeLnk * copyNode) {
-    
     NodeLnk* node = nullptr;
 
     if(copyNode!=nullptr){
-        
         node = new NodeLnk(copyNode->data);
-
         if(copyNode->leftChild!=nullptr) {
             node->leftChild = recursiveCopyTree(copyNode->leftChild);
         }
-
         if(copyNode->rightChild!=nullptr) {
             node->rightChild = recursiveCopyTree(copyNode->rightChild);
         }
     }
-
     return node;
 }
 
 template <typename Data>
 BinaryTreeLnk<Data>::BinaryTreeLnk(const BinaryTreeLnk<Data> &binaryTreeLnk) {
-    
     size=binaryTreeLnk.size;
-
     if (size>0) {
-        
         root = recursiveCopyTree(binaryTreeLnk.root);
     }
 }
 
 template <typename Data>
 BinaryTreeLnk<Data>::BinaryTreeLnk(BinaryTreeLnk<Data> &&binaryTreeLnk) noexcept {
-    
     std::swap(root,binaryTreeLnk.root);
     std::swap(size,binaryTreeLnk.size);
 }
 
 template <typename Data>
 BinaryTreeLnk<Data>& BinaryTreeLnk<Data>::operator=(const BinaryTreeLnk<Data> &binaryTreeLnk) {
-    
     if(binaryTreeLnk.size!=0) {
-        
         BinaryTreeLnk<Data>* tmpBinaryTreeLnk = new BinaryTreeLnk<Data>(binaryTreeLnk);
-	    
         std::swap(*tmpBinaryTreeLnk, *this);
-	    
         delete tmpBinaryTreeLnk;
-
     } else{
-
         Clear();
     }
-
     return *this;
 }
 
 template <typename Data>
 BinaryTreeLnk<Data>& BinaryTreeLnk<Data>::operator=(BinaryTreeLnk<Data> &&binaryTreeLnk) noexcept {
-    
     std::swap(root,binaryTreeLnk.root);
     std::swap(size,binaryTreeLnk.size);
-    
     return *this;
 }
 
@@ -238,31 +197,24 @@ bool BinaryTreeLnk<Data>::operator!=(const BinaryTreeLnk<Data> &binaryTreeLnk) c
 
 template <typename Data>
 BinaryTreeLnk<Data>::NodeLnk& BinaryTreeLnk<Data>::Root() const {
-
     if(root==nullptr) {
         throw std::length_error("Empty BinaryTreeLnk");
     }
-
     return *root;
 }
 
 template <typename Data>
 BinaryTreeLnk<Data>::NodeLnk& BinaryTreeLnk<Data>::Root() {
-
     if(root==nullptr) {
         throw std::length_error("Empty BinaryTreeLnk");
     }
-
     return *root;
 }
 
 template <typename Data>
 void BinaryTreeLnk<Data>::Clear() noexcept {
-    
     size=0;
-    
-    delete root;
-    
+    delete root; //It deallocates the entire tree, as NodeLnk's destructor takes care of deallocating the children.
     root = nullptr;
 }
 /* ************************************************************************** */
